ab3f80a78ad3c11c8e285510f3041dc92854df14
Katie Fitz <fitzsi28@gmail.com>
2015-05-20 17:41:20 -0500

    one full cycle working

--- test.py
+++ test.py
@@ -11,17 +11,22 @@ import trep.discopt
 
 ## System parameters: 
 
-m = 0.015 # Mass of pendulum
-l = 3.0 # Length of pendulum
-q0 = 0.3 # Initial angle of pendulum
-dq0 = -0.510 # Initial velocity of pendulum
+m = 12 # Mass of pendulum
+l = 1.0 # Length of pendulum
+
 t0 = 0.0 # Initial time
 tf = 4.0 # Final time
 dt = 0.01 # Timestep
-B = 0.1 # Damping coefficient
-g = 9.8
-tau = 1.2*np.sqrt(g/l) #time constant from KUO
-u = 2.5#impulse force
+B = 8.4 # Damping coefficient
+g = 9.81 #potential due to gravity and springs
+
+
+#From KUO
+tau = 0.42 #time constant from KUO
+q0 = 0.3 # Initial angle of pendulum
+dq0 = -0.510*np.sqrt(g/l) # Initial velocity of pendulum
+#print 1.2/np.sqrt(g/l)
+u = 1575.0#impulse force
 
 
 system = trep.System() # Initialize system
@@ -36,25 +41,30 @@ trep.potentials.Gravity(system, (0, 0, -g)) # Add gravity
 trep.forces.Damping(system, B) # Add damping
 trep.forces.ConfigForce(system, 'theta', 'theta-torque') # Add input (externally applied torque)
 
+system.q = q0
+system.dq = dq0
 ## Create and initialize the variational integrator
 mvi = trep.MidpointVI(system)
-mvi.initialize_from_configs(t0, np.array([q0]), t0+dt, np.array([q0+dq0*dt]))
-# mvi.initialize_from_state ()
-# generalized momentum, need to calculate from the legandgre transform
+#mvi.initialize_from_configs(t0, np.array([q0]), t0+dt, np.array([q0+dq0*dt]))
+mvi.initialize_from_state(t0,np.array([q0]),np.array([m*l*dq0]))
+
 
 #feed-forward force function
 def FF(time):
     if(time>(tau-dt) and time<(tau+dt)):
        tor=u
+      
     elif(time>(2*tau-dt) and time<(2*tau+dt)):
        tor=-1*u
+       
     else:
        tor = 0
+    
     return[tor]
 
 ## Simulate system forward in time
 T = [mvi.t1] # List to hold time values
-Q = [mvi.q1] # List to hold configuration values
+Q = [system.q] # List to hold configuration values
 dQ = [system.dq] # List to hold velocities 
 U = [0.0]
 
@@ -63,6 +73,8 @@ while mvi.t1 < tf:
     torque[0] = FF(mvi.t2+dt)[0] #+SAC <--plug this into mvi where [0.0] is
     #print "torque= ", torque
     mvi.step(mvi.t2+dt, u1 = torque) # Step the system forward by one time step
+    if(mvi.t1>(tau-dt) and mvi.t1<(tau+dt)):
+       print "dq0 = ",dq0,"system.dq = ",system.dq
     T.append(mvi.t1)
     Q.append(mvi.q1)
     dQ.append(system.dq)
@@ -70,13 +82,13 @@ while mvi.t1 < tf:
 
 
 # Plot results
-ax1 = pylab.subplot(211,autoscale_on = False,xlim =[-0.4,0.4],ylim =[-0.6,0.6])
+ax1 = pylab.subplot(211,autoscale_on = False,xlim =[-0.4,0.4],ylim =[-1.75,1.75])
 pylab.plot(Q, dQ)
 pylab.title("FeedForward Controller")
 pylab.ylabel("dtheta")
 pylab.xlabel("theta")
 pylab.legend(["qd","p"])
-pylab.subplot(212,autoscale_on=False,xlim=[0,5],ylim=[-0.6,0.6])
+pylab.subplot(212,autoscale_on=False,xlim=[0,5],ylim=[-1.75,1.75])
 pylab.plot(T,Q)
 pylab.plot(T,dQ)
 pylab.plot(T, U)
@@ -85,7 +97,7 @@ pylab.legend(["Q","dQ","U"])
 pylab.show()
 
 ## Visualize the system in action
-trep.visual.visualize_3d([ trep.visual.VisualItem3D(system, T, Q) ])
+#trep.visual.visualize_3d([ trep.visual.VisualItem3D(system, T, Q) ])
 
 
 """
