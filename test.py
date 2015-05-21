8245b11aa7d80f3d3b13a1b28f113f0a9c69b3af
Katie Fitz <fitzsi28@gmail.com>
2015-05-21 17:03:50 -0500

    tuned feedforward trep

--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 import numpy as np
 import trep
 from trep import tx,ty,tz,rx,ry,rz
-import pylab 
+from pylab import *
 from numpy import dot
 import trep.discopt
 
@@ -15,18 +15,18 @@ m = 12 # Mass of pendulum
 l = 1.0 # Length of pendulum
 
 t0 = 0.0 # Initial time
-tf = 4.0 # Final time
+tf = 2.0 # Final time
 dt = 0.01 # Timestep
 B = 8.4 # Damping coefficient
 g = 9.81 #potential due to gravity and springs
 
 
 #From KUO
-tau = 0.42 #time constant from KUO
+tau = 0.398 #time constant from KUO
 q0 = 0.3 # Initial angle of pendulum
 dq0 = -0.510*np.sqrt(g/l) # Initial velocity of pendulum
 #print 1.2/np.sqrt(g/l)
-u = 1575.0#impulse force
+u = 3195.0#impulse force
 
 
 system = trep.System() # Initialize system
@@ -48,19 +48,32 @@ mvi = trep.MidpointVI(system)
 #mvi.initialize_from_configs(t0, np.array([q0]), t0+dt, np.array([q0+dq0*dt]))
 mvi.initialize_from_state(t0,np.array([q0]),np.array([m*l*dq0]))
 
-
+prev = 0
 #feed-forward force function
-def FF(time):
-    if(time>(tau-dt) and time<(tau+dt)):
+def FF(t,prev):
+    time = t-prev
+    if(time>(tau-0.005) and time<(tau+0.005)):
        tor=u
-      
-    elif(time>(2*tau-dt) and time<(2*tau+dt)):
+       #prev=t
+       #print time
+       #print "impulse1 =", m*l*(dq0-system.dq)/dt
+    elif(time>(2*tau-0.005) and time<(2*tau+0.005)):
        tor=-1*u
-       
+       #prev = t
+       #print time
+       #print "impulse2 =", m*l*l*((-1*dq0)-system.dq)/dt
+    elif(time>(3*tau-0.005) and time<(3*tau+0.005)):
+       tor=u
+       #print time
+       #print "impulse3 =", m*l*l*(dq0-system.dq)/dt
+    elif(time>(4*tau-0.005) and time<(4*tau+0.005)):
+       tor=-1*u
+       #print time
+       #print "impulse4 =", m*l*l*(-1*dq0-system.dq)/dt
     else:
        tor = 0
     
-    return[tor]
+    return[tor,prev]
 
 ## Simulate system forward in time
 T = [mvi.t1] # List to hold time values
@@ -70,11 +83,19 @@ U = [0.0]
 
 while mvi.t1 < tf:
     torque = np.zeros(system.nu)
-    torque[0] = FF(mvi.t2+dt)[0] #+SAC <--plug this into mvi where [0.0] is
-    #print "torque= ", torque
+    control_val = FF(mvi.t2+dt,prev)
+    torque[0] = control_val[0] #+SAC <--plug this into mvi where [0.0] is
+    prev = control_val[1]
+    #print "torque= ", FF(mvi.t2+dt,prev)
+    #mvi.step(mvi.t2+dt, [0.0]) # Step the system forward by one time step
     mvi.step(mvi.t2+dt, u1 = torque) # Step the system forward by one time step
-    if(mvi.t1>(tau-dt) and mvi.t1<(tau+dt)):
-       print "dq0 = ",dq0,"system.dq = ",system.dq
+    if(mvi.q1>(0.3-0.005) and mvi.q1<(0.3+0.005)):
+       print "mvi.t1", mvi.t1
+       print "mvi.q1", mvi.q1
+    if(mvi.q1>(-0.3-0.005) and mvi.q1<(-0.3+0.005)):
+       print "mvi.t1", mvi.t1
+       print "mvi.q1", mvi.q1
+   
     T.append(mvi.t1)
     Q.append(mvi.q1)
     dQ.append(system.dq)
@@ -82,40 +103,21 @@ while mvi.t1 < tf:
 
 
 # Plot results
-ax1 = pylab.subplot(211,autoscale_on = False,xlim =[-0.4,0.4],ylim =[-1.75,1.75])
-pylab.plot(Q, dQ)
-pylab.title("FeedForward Controller")
-pylab.ylabel("dtheta")
-pylab.xlabel("theta")
-pylab.legend(["qd","p"])
-pylab.subplot(212,autoscale_on=False,xlim=[0,5],ylim=[-1.75,1.75])
-pylab.plot(T,Q)
-pylab.plot(T,dQ)
-pylab.plot(T, U)
-pylab.xlabel("T")
-pylab.legend(["Q","dQ","U"])
-pylab.show()
+ax1 = subplot(211,autoscale_on = False,xlim =[-0.4,0.4],ylim =[-1.75,1.75])
+plot(Q, dQ)
+title("FeedForward Controller")
+ylabel("dtheta")
+xlabel("theta")
+legend(["qd","p"])
+subplot(212,autoscale_on=False,xlim=[0,tf],ylim=[-1.75,1.75])
+plot(T,Q)
+plot(T,dQ)
+plot(T, U)
+xlabel("T")
+legend(["Q","dQ","U"])
+show()
 
 ## Visualize the system in action
 #trep.visual.visualize_3d([ trep.visual.VisualItem3D(system, T, Q) ])
 
-
-"""
-## Phase Plane Limit Cycel
-plot(Q, dQ)
-ylabel(" dtheta ")
-xlabel(" theta ")
-show()
-
-plot(T, Q)
-plot(T,U)
-ylabel(" theta ")
-xlabel(" time ")
-show()
-
-
-## Visualize the system in action
-trep.visual.visualize_3d([ trep.visual.VisualItem3D(system, T, Q) ])
-
-"""
 
